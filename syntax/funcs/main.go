517a1bd2478e7bbcef0cc858287b82cb31347fa3
hcjjj <hcjjj@foxmail.com>
2024-03-16 09:01:16 +0800

    基于 Redis 的 IP 限流；利用 User-Agent 增强登录安全性

--- syntax/funcs/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func main() {
-	res := DeferReturn()
-	println(res)
-	println(DeferReturnV1())
-	println(DeferReturnV2().name)
-}
--- syntax/hello_go/hello.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func Hello() {
-	println("调用了 hello, Go!")
-}
